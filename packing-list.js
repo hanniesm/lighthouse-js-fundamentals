5c2196be9af7a848178c52d22b6d530549728f7d
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-02-23 18:02:45 +0000

    Adds looping exercises

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+var packingList = ["bowls", "plates", "pots", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pakc:");
+  for (var i = 0; i < packingList.length; i++) {
+    console.log(packingList[i]);
+  }
