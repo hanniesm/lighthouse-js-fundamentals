5107f91a273c8f17f7e2b8cd1b4cb8fabdf4ae5f
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-02-23 18:15:57 +0000

    Adds problem with looping

---
+++ ingredients.js
@@ -0,0 +1,16 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var i = 0
+while (i < 8) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (var i = 7; i > 0; i--) {
+ console.log(ingredients[i]);
+}
