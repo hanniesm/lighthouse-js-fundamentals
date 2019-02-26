d9bbc822edb6d87bdf264aaf169fab34e7fdb36f
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-02-26 20:36:16 +0000

    Add ageCalculator exercise

--- ageCalculator.js
+++ ageCalculator.js
@@ -5,3 +5,12 @@ function ageCalculator(name, yearOfBirth, currentYear) {
 
 console.log(ageCalculator("Miranda", 1983, 2015));
 console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+function ageCalculator(name, yearOfBirth, currentYear) {
+  return name + " is " + (currentYear - yearOfBirth) + " years old."
+}
+
+console.log(ageCalculator2("Amanda", 1983, 2015));
+console.log(ageCalculator2("Donald", 1988, 2015));
+
+
