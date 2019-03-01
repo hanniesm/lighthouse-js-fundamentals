75b0705e3e69c0db878bd586e85991bf1ef9cfd1
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-03-01 16:22:50 +0000

    More practice with conditionals

---
+++ walk-pixel.js
@@ -0,0 +1,14 @@
+var raining = true;
+var poop = false;
+
+if (!poop) {
+  console.log("Whatever the weather, you have to go out");
+  } else {
+    if (!raining) {
+      console.log("You might as well go out and enjoy the sun!");
+    } else {
+      console.log("Phew! You don't have to go out right now");
+    }
+  }
+
+
