ccab6750194299841c1c391c9d30429254ffa251
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-03-01 17:03:44 +0000

    Improvements to a few of the js fundamentals exercises

--- chorus-while.js
+++ chorus-while.js
@@ -5,3 +5,14 @@ while (repeat < 10) {
   repeat++;
 }
 console.log("Until the sun comes up!");
+
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
--- ingredients.js
+++ ingredients.js
@@ -5,11 +5,13 @@ while (i < 8) {
   console.log(ingredients[i]);
   i++;
 }
+console.log("\n");
 
 
 for (var i = 0; i < ingredients.length; i++) {
   console.log(ingredients[i]);
 }
+console.log("\n");
 
 for (var i = 7; i > 0; i--) {
  console.log(ingredients[i]);
