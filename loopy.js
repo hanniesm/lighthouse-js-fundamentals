a2e2e0cda14c1bc4c496d2d484154702332848d5
Hannah <hanniesutton@gmail.com>
2019-03-20 15:12:04 +0000

    Refactored loopy.js

--- loopy.js
+++ loopy.js
@@ -1,14 +1,14 @@
-for (let counter = 100; counter <= 200; counter++) {
-  if (counter % 3 === 0 && counter % 4 === 0) {
-    console.log("LoopyLighthouse")
-  } else if (counter % 3 === 0) {
-    console.log("Loopy");
-  } else if (counter % 4 === 0) {
-    console.log("Lighthouse");
-  } else {
-    console.log(counter);
-}
-}
+// for (let counter = 100; counter <= 200; counter++) {
+//   if (counter % 3 === 0 && counter % 4 === 0) {
+//     console.log("LoopyLighthouse")
+//   } else if (counter % 3 === 0) {
+//     console.log("Loopy");
+//   } else if (counter % 4 === 0) {
+//     console.log("Lighthouse");
+//   } else {
+//     console.log(counter);
+// }
+// }
 
 const loopyLighthouse = function(range, multiples, words) {
   for (let counter = range[0]; counter <= range[1]; counter++) {
@@ -26,3 +26,4 @@ const loopyLighthouse = function(range, multiples, words) {
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 loopyLighthouse([100, 200], [3, 4], ["Argh", "Banana"]);
+
